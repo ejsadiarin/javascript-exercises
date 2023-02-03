9b76028d494f5835257a53a9eb44f0be4acca56f
ejsadiarin <ejsadiarin@gmail.com>
2023-02-03 23:24:50 +0800

    02 done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,19 @@
-const repeatString = function() {
+const repeatString = function(word, repeatNumber) {
 
+    if (repeatNumber < 0) return "ERROR";
+    if (word === "") return "";
+    
+    let result = "";
+    for (let i = 0; i < repeatNumber; i++) {
+        result =  result + word;
+    }
+    return result;
+
+    // optimized solution with .repeat() string method
+    // const result = word.repeat(repeatNumber);
+    // return result;
 };
+// console.log(repeatString('hey', 4));
 
 // Do not edit below this line
 module.exports = repeatString;
